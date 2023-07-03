75bcd5f202866c4e508c16cd98d0c8831e4c7fa3
lilla-k <kalmarlilianna@gmail.com>
2023-07-03 07:58:03 +0200

    categories added more detailed

--- src/categories.js
+++ src/categories.js
@@ -1,51 +1,110 @@
 const categories = [{
-    name: "T-shirts",
-    img: "/images/categories/t-shirt.png",
-    subCategories: [{
-        name: "long sleeve t-shirts", 
-        img:"/images/categories/long-sleeve.png"},{
-        name: "short sleeve t-shirts",
-        img:"/images/categories/t-shirt.png" }, {
-        name: "tops",
-        img:"/images/categories/tank-top.png"
-        }]
-  }, 
-  {
-    name: "Jeans",
-    img: "/images/categories/jeans.png",
-    subCategories: [{
-        name: "jeans", 
-        img:"/images/categories/jeans.png"},{
-        name: "shorts",
-        img:"/images/categories/shorts.png" }, {
-        name: "leggings",
-        img:"/images/categories/leggings.png"
-        }]
-  },
-  {
+  name: "T-shirts",
+  img: "/images/categories/t-shirt.png",
+  subCategories: [{
+    name: "Long Sleeve T-shirts",
+    img: "/images/categories/long-sleeve.png"
+  }, {
+    name: "Short Sleeve T-shirts",
+    img: "/images/categories/t-shirt.png"
+  }, {
+    name: "Tops",
+    img: "/images/categories/tank-top.png"
+  }]
+},
+{
+  name: "Trousers",
+  img: "/images/categories/jeans.png",
+  subCategories: [{
+    name: "Long Trousers",
+    img: "/images/categories/jeans.png"
+  }, {
+    name: "Shorts",
+    img: "/images/categories/shorts.png"
+  }, {
+    name: "Leggings",
+    img: "/images/categories/leggings.png"
+  }]
+},
+{
+  name: "Sweaters",
+  img: "/images/categories/sweater.png",
+  subCategories: [{
     name: "Sweater",
-    img: "/images/categories/sweater.png",
-  },
-  {
-    name: "Coat",
-    img: "/images/categories/coat.png",
-  },
-  {
-    name: "Dress",
-    img: "/images/categories/dress.png",
-  },
-  {
-    name: "Shoes",
-    img: "/images/categories/jeans.png",
-  },
-  {
-    name: "Skirt",
-    img: "/images/categories/skirt.png",
-  },
-  {
-    name: "Pajamas",
-    img: "/images/categories/pajamas.png",
-  },
-  ];
+    img: "/images/categories/sweater.png"
+  }, {
+    name: "Blazers",
+    img: "/images/categories/blazer.png"
+  }]
+},
+{
+  name: "Coats",
+  img: "/images/categories/coat.png",
+},
+{
+  name: "Dresses",
+  img: "/images/categories/dress.png",
+  subCategories: [{
+    name: "Long Sleeve Dresses",
+    img: "/images/categories/long-sleeve.png"
+  }, {
+    name: "Short Sleeve dresses",
+    img: "/images/categories/t-shirt.png"
+  }, {
+    name: "Evening Dresses",
+    img: "/images/categories/tank-top.png"
+  }]
+},
+{
+  name: "Shoes",
+  img: "/images/categories/jeans.png",
+  subCategories: [{
+    name: "Half Shoes",
+    img: "/images/categories/jeans.png"
+  }, {
+    name: "Boots",
+    img: "/images/categories/shorts.png"
+  }, {
+    name: "High-hells",
+    img: "/images/categories/leggings.png"
+  },  {
+    name: "Other Shoes",
+    img: "/images/categories/leggings.png"
+  }]
+},
+{
+  name: "Skirts",
+  img: "/images/categories/skirt.png",
+},
+{
+  name: "Pajamas",
+  img: "/images/categories/pajamas.png",
+},
+{
+  name: "Bags",
+  img: "/images/categories/bag.png",
+  subCategories: [{
+    name: "Backpacks",
+    img: "/images/categories/backpack.png"
+  }, {
+    name: "Shoulder Bags",
+    img: "/images/categories/bag.png"
+  }, {
+    name: "Clutches",
+    img: "/images/categories/bag.png"
+  }]
+},
+{
+  name: "Accessories",
+  img: "/images/categories/accessory.png",
+  subCategories: [{
+    name: "Scarfs",
+    img: "/images/categories/scarf.png"
+  }, {
+    name: "Hats",
+    img: "/images/categories/hat.png"
+  }]
+},
+];
 
-  export default categories;
+export default categories;
