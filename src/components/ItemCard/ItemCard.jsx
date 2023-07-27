e45568fcb96671c6af5ef5a33aa2f6b48aa6bde3
lilla-k <kalmarlilianna@gmail.com>
2023-07-27 07:25:53 +0200

    [5] favorite and not favorite are separated

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -2,7 +2,8 @@ import './ItemCard.css'
 
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faHeart as solidfaHeart} from '@fortawesome/free-solid-svg-icons'
+import { faHeart as regularfaHeart } from '@fortawesome/free-regular-svg-icons'
 import { useParams } from 'react-router-dom';
 
 function ItemCard(props){
@@ -15,7 +16,8 @@ function ItemCard(props){
         <Link to={`/categories/${categoryId}/${subCategoryId}/cloths/${props.id}`}>
           <div className="ItemCard-imageContainer">
             <img src={props.image} className="ItemCard-image" />
-            <span className="ItemCard-favorite"><FontAwesomeIcon icon={faHeart} /></span>
+            {props.favorite &&  <span className="ItemCard-favorite"><FontAwesomeIcon icon={solidfaHeart} /></span>}
+            {!props.favorite && <span className="ItemCard-favorite"><FontAwesomeIcon icon={regularfaHeart} /></span>}
             <div className="ItemCard-usage">{props.usage}</div>
           </div>
           <div className="ItemCard-name">{props.name}</div>
