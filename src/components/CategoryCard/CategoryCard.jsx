10a2bff5b6326412b8f33a4cea380452c55ecade
lilla-k <kalmarlilianna@gmail.com>
2023-08-01 12:58:54 +0200

    [10 item numbers added on the subCategory cards

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -1,14 +1,23 @@
 import './CategoryCard.css'
+import cloths from '../../cloths.js'
 
 import { Link } from "react-router-dom";
 
 function CategoryCard(props) {
+  debugger;
+  console.log(cloths);
+  const subCategoryItemsNumber = cloths.filter(element => {
+    debugger;
+    return element.subCategory===props.subCategoryName}).length;
+
+  console.log(subCategoryItemsNumber);
 
   return (
     <div className="CategoryCard">
       <Link to={`/categories/${props.categoryName}/${props.subCategoryName || ""}`}>
         <img src={props.image} className="CategoryCard-image" />
         <div className="CategoryCard-name">{props.subCategoryName || props.categoryName}</div>
+        {props.subCategoryName && <div className="CategoryCard-itemsNumber">{subCategoryItemsNumber} items</div>}
       </Link>
     </div>
 
