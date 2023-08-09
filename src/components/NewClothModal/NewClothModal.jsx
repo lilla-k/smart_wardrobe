ca45db99fbc14ba91ad471a169189aca5801d3c4
lilla-k <kalmarlilianna@gmail.com>
2023-08-09 08:09:48 +0200

    [12] choosable categories in NewClothModal

--- src/components/NewClothModal/NewClothModal.jsx
+++ src/components/NewClothModal/NewClothModal.jsx
@@ -1,22 +1,35 @@
 import './NewClothModal.css'
+import categories from '../../categories';
+import {useState} from 'react';
 
 function NewClothModal(props){
 
+    const [selectedCategory, setSelectedCategory] = useState("");
+    
     const closeHandler=(event)=>{
         if(event.target.className==="NewClothModal-backdrop"){
             props.onClose();
         }
     }
+    
+    const selectedCategoryIndex=categories.findIndex((category) => category.name === selectedCategory);
+    console.log(selectedCategoryIndex);
 
     return(
         <div>
             <div className="NewClothModal-backdrop" onClick= {closeHandler}>
                 <div className="NewClothModal-modal">
                 <div className="NewClothModal-closeButton" onClick= {props.onClose}>x</div>
-                <div>Choose category:</div>
-        
-                <div>Choose subcategory:</div>
-
+                <label>Choose category:</label>
+                <select onChange={event=>setSelectedCategory(event.target.value)}>
+                    <option value=""></option>
+                    {categories.map((category) => <option value={category.name}>{category.name}</option>)}
+                </select>
+                <label>Choose subcategory:</label>
+                <select>
+                    <option value=""></option>
+                    {selectedCategoryIndex >-1 && categories[selectedCategoryIndex].subCategories?.map(subcategory => <option>{subcategory.name}</option>)}
+                </select>
             </div>
             </div>
 
