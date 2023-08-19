5ce648cd01fd977615077210b2dad18b67ac820c
lilla-k <kalmarlilianna@gmail.com>
2023-08-19 22:56:45 +0200

    get cloths from api

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3001
-
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3001;
+const cors = require('cors');
 const fs = require('fs');
+const clothsFilePath = path.join(__dirname, 'cloths.json');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json());
+app.use(cors());
+
+app.get('/api/cloths', (req, res) => {
+  res.sendFile(clothsFilePath);
 })
 
 app.listen(port, () => {
--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -3,54 +3,25 @@ import Breadcrumbs from '../Breadcrumbs/Breadcrumbs'
 import NewClothModal from '../NewClothModal/NewClothModal';
 
 import { Outlet } from "react-router-dom";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import './Root.css'
 
 function Root() {
+
+  const [cloths, setCloths] = useState([]);
   
-  const [cloths, setCloths] = useState([{
-    "id":1,
-    "subCategory": "Long Sleeve T-shirts",
-    "name": "Julie, single coloured T-shirt",
-    "brand": "Only",
-    "color": "purple",
-    "material": "80% viscose, 20% polyamide",
-    "purchaseYear": 2023,
-    "purchaseMonth": 3,
-    "price": 9500,
-    "image": "/images/cloths/2023_03_07_Only_lila_hosszu_polo.avif",
-    "favorite": false,
-    "usage": 5
-  },
-  {
-    "id": 2,
-    "subCategory": "Long Sleeve T-shirts",
-    "name": "Julie, single coloured T-shirt",
-    "brand": "Only",
-    "color": "green",
-    "material": "80% viscose, 20% polyamide",
-    "purchaseYear": 2023,
-    "purchaseMonth": 3,
-    "price": 9500,
-    "image": "/images/cloths/2023_03_07_Only_zold_hosszu_polo.avif",
-    "favorite": true,
-    "usage": 4
-  },
-  {
-    "id":3,
-    "subCategory": "Short Sleeve T-shirts",
-    "name": "leaf pattern T-shirt",
-    "brand": "Only",
-    "color": "pink",
-    "material": "100% cotton",
-    "purchaseYear": 2023,
-    "purchaseMonth": 6,
-    "price": 5300,
-    "image": "/images/cloths/2023_06_28_Only_rozsaszin_polo_20230628.avif",
-    "favorite": false,
-    "usage": 2
-  }]);
+  async function loadCloths () {
+    const response = await fetch("http://localhost:3001/api/cloths");
+    const data = await response.json();
+    console.log(data);
+    setCloths(data);
+  }
+
+  useEffect(() => {
+    loadCloths();
+  }, []);
+
 
   const [openModal, setOpenModal] = useState(false);
 
